cad64fa71d436a1a860fa1dc76eac82f76042d6b
Casey Hald <caseyhald@Caseys-MacBook-Pro.local>
2020-05-06 16:56:31 -0700

    added typescript state to component

--- src/components/signInModal.tsx
+++ src/components/signInModal.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { useForm } from "react-hook-form";
 import {connect,useSelector, useDispatch} from 'react-redux'
 import {
   getAirtableApi,
@@ -11,13 +12,38 @@ import AirmaLogo from '../assets/airma-logo.svg'
 import SignInHeader from "./SignInHeader";
 import '../ui.css'
 
-class SignInModal extends Component {
+type MyProps = {};
+type MyState = { value: string };
+
+class SignInModal extends Component<MyProps, MyState> {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
   render() {
 
     return (
       <div className="airtable__container">
         <SignInHeader />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
